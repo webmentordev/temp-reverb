9d8a7f7ff6244a411aefdab261d2d7bca295af60
webmentordev <ahmertahir99@gmail.com>
2025-08-19 11:53:07 +0500

    Temp Repo, Nginx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.app')
 @section('content')
-
+    <div class="max-w-3xl w-full m-auto py-12" x-data="" x-init="Echo.channel('room')">
+        <h1>Welcome to the Reverb Home page!</h1>
+    </div>
 @endsection
